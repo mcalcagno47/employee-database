9272d16659fddffa821b63246988bec0929ae673
Mark Calcagno <mcalcagn@gmail.com>
2022-12-18 22:37:22 -0600

    changed syntax, still issues with employee table

--- db/schema.sql
+++ db/schema.sql
@@ -4,39 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(100) NOT NULL
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    salary INT NOT NULL,
-    department_id INT NOT NULL,
-    -- PRIMARY KEY department(id),
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-);
-
-CREATE TABLE manager (
-    id INT NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    roles_id INT NOT NULL,
-    PRIMARY KEY department(id),
-    FOREIGN KEY (roles_id)
-    REFERENCES roles(id)
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    roles_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY department(id),
-    FOREIGN KEY (roles_id)
-    REFERENCES roles(id),
-    FOREIGN KEY (manager_id)
-    REFERENCES manager(id)
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,18 @@
 INSERT INTO department (id, department_name)
-VALUES (01, 'Engineering'),
-    (02, 'Finance'),
-    (03, 'Legal'),
-    (04, 'Sales'),
+VALUES ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales'),
 
 INSERT INTO roles (id, title, salary, department_id)
-VALUES (01, 'Sales Lead', 85000, 04),
-    (02, 'Salesperson', 70000, 04),
-    (03, 'Lead Engineer', 95000, 01),
-    (04, 'Software Engineer', 85000, 01),
-    (05, 'Account Manager', 120000, 02),
-    (06, 'Accountant', 100000, 02),
-    (07, 'Legal Team Lead', 210000, 03),
-    (08, 'Lawyer', 180000, 03);
-
-INSERT INTO manager (id, first_name, last_name, roles_id)
-VALUES (1, 'Ben', 'Sisko', 07),
-    (2, 'Jean Luc', 'Picard', 01),
-    (3, 'Kathryn', 'Janeway', 03),
-    (4, 'Christopher', 'Pike', 05),
-    (5, 'Carol', 'Freeman', 07);
+VALUES ('Sales Lead', 85000, 4),
+    ('Salesperson', 70000, 4),
+    ('Lead Engineer', 95000, 1),
+    ('Software Engineer', 85000, 1),
+    ('Account Manager', 120000, 2),
+    ('Accountant', 100000, 2),
+    ('Legal Team Lead', 210000, 3),
+    ('Lawyer', 180000, 3);
 
 INSERT INTO employee (id, first_name, last_name, roles_id, manager_id)
 VALUES (001, 'John', 'Doe', 01, 1),
