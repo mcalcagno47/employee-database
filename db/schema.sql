fe3af6bdb63269b0fbb5a19e85b0eaec5248930d
Mark Calcagno <mcalcagn@gmail.com>
2022-12-19 15:05:42 -0600

    cannot identify issues with seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+  name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE roles (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
+  name VARCHAR(50) UNIQUE NOT NULL,
+  salary INT UNSIGNED NOT NULL,
   department_id INT UNSIGNED NOT NULL,
   INDEX dep_ind (department_id),
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
@@ -19,8 +19,8 @@ CREATE TABLE roles (
 
 CREATE TABLE employees (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   roles_id INT UNSIGNED NOT NULL,
   INDEX roles_ind (roles_id),
   CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,26 @@
-INSERT INTO department (id, department_name)
-VALUES ('Engineering'),
-    ('Finance'),
-    ('Legal'),
-    ('Sales'),
+INSERT INTO department (department_name)
+VALUES ("Engineering"),
+    ("Finance"),
+    ("Legal"),
+    ("Sales"),
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES ('Sales Lead', 85000, 4),
-    ('Salesperson', 70000, 4),
-    ('Lead Engineer', 95000, 1),
-    ('Software Engineer', 85000, 1),
-    ('Account Manager', 120000, 2),
-    ('Accountant', 100000, 2),
-    ('Legal Team Lead', 210000, 3),
-    ('Lawyer', 180000, 3);
+INSERT INTO roles (name, salary, department_id)
+VALUES ("Sales Lead", 85000, 4),
+    ("Salesperson", 70000, 4),
+    ("Lead Engineer", 95000, 1),
+    ("Software Engineer", 85000, 1),
+    ("Account Manager", 120000, 2),
+    ("Accountant", 100000, 2),
+    ("Legal Team Lead", 210000, 3),
+    ("Lawyer", 180000, 3);
 
-INSERT INTO employees (id, first_name, last_name, roles_id, manager_id)
-VALUES ('John', 'Doe', 01, 1),
-    ('Rebecca', 'Gomez', 01, 1),
-    ('George', 'Kleet', 02, 1),
-    ('Mary', 'Rosenbaum', 03, 1),
-    ('Mike', 'Rotch', 04, 2),
-    ('Amanda', 'Rosenbaum', 05, 3),
-    ('William', 'Runger', 06, 4),
-    ('Jeff', 'Klein', 07, 5);
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+VALUES ("John", "Doe", 01, 1),
+    ("Rebecca", "Gomez", 01, 1),
+    ("George", "Kleet", 02, 1),
+    ("Mary", "Rosenbaum", 03, 1),
+    ("Mike", "Rotch", 04, 2),
+    ("Amanda", "Rosenbaum", 05, 3),
+    ("William", "Runger", 06, 4),
+    ("Jeff", "Klein", 07, 5);
     
