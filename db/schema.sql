7a0f7dba006ea345e6144a162faab6c7ec372213
Mark Calcagno <mcalcagn@gmail.com>
2022-12-18 19:12:11 -0600

    diagnosing issues with 1064 error:

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,25 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  dept_name VARCHAR(30) NOT NULL
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id),
-  REFERENCES department(id),
-  ON DELETE SET NULL
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  FOREIGN KEY (role_id),
-  REFERENCES role(id),
-  manager_id INT,
-  FOREIGN KEY (manager_id),
-  REFERENCES manager(id),
-  ON DELETE SET NULL
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (roles_id),
+    FOREIGN KEY (manager_id)
 );
